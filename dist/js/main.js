4c120aeead8b2a91332f3dc43b779922dbcf721d
robotcigan <robotcigan@gmail.com>
2018-12-15 16:41:29 +0300

    some

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,21 +1,43 @@
-"use strict";
+'use strict';
 
-var questions = [{
-  "id": "1",
-  "question": "Тегістегіш-жоңғыштар кездесетін неолиттік Қараүңгір тұрағы табылған аймақ",
-  "type": "radio",
-  "options": [{ "optionName": "1Шығыс Қазақстан", "correct": false }, { "optionName": "Орталық Қазақстан", "correct": false }, { "optionName": "Арал маңы", "correct": true }, { "optionName": "Оңтүстік Қазақстан", "correct": false }, { "optionName": "Жетісу", "correct": false }]
-}, {
-  "id": "2",
-  "question": "Тегістегіш-жоңғыштар кездесетін неолиттік Қараүңгір тұрағы табылған аймақ",
-  "type": "radio",
-  "options": [{ "optionName": "2Шығыс Қазақстан", "correct": false }, { "optionName": "Орталық Қазақстан", "correct": false }, { "optionName": "Арал маңы", "correct": true }, { "optionName": "Оңтүстік Қазақстан", "correct": false }, { "optionName": "Жетісу", "correct": false }]
-}, {
-  "id": "3",
-  "question": "Тегістегіш-жоңғыштар кездесетін неолиттік Қараүңгір тұрағы табылған аймақ",
-  "type": "checkbox",
-  "options": [{ "optionName": "4Шығыс Қазақстан", "correct": true }, { "optionName": "Орталық Қазақстан", "correct": true }, { "optionName": "Арал маңы", "correct": true }, { "optionName": "Оңтүстік Қазақстан", "correct": false }, { "optionName": "Жетісу", "correct": false }]
-}];
+// var questions = [
+//   {
+//     "id": "1",
+//     "question": "Тегістегіш-жоңғыштар кездесетін неолиттік Қараүңгір тұрағы табылған аймақ",
+//     "type": "radio",
+//     "options": [
+//       {"optionName": "1Шығыс Қазақстан", "correct": false},
+//       {"optionName": "Орталық Қазақстан", "correct": false},
+//       {"optionName": "Арал маңы", "correct": true},
+//       {"optionName": "Оңтүстік Қазақстан", "correct": false},
+//       {"optionName": "Жетісу", "correct": false}
+//     ]
+//   },
+//   {
+//     "id": "2",
+//     "question": "Тегістегіш-жоңғыштар кездесетін неолиттік Қараүңгір тұрағы табылған аймақ",
+//     "type": "radio",
+//     "options": [
+//       {"optionName": "2Шығыс Қазақстан", "correct": false},
+//       {"optionName": "Орталық Қазақстан", "correct": false},
+//       {"optionName": "Арал маңы", "correct": true},
+//       {"optionName": "Оңтүстік Қазақстан", "correct": false},
+//       {"optionName": "Жетісу", "correct": false}
+//     ]
+//   },
+//   {
+//     "id": "3",
+//     "question": "Тегістегіш-жоңғыштар кездесетін неолиттік Қараүңгір тұрағы табылған аймақ",
+//     "type": "checkbox",
+//     "options": [
+//       {"optionName": "4Шығыс Қазақстан", "correct": true},
+//       {"optionName": "Орталық Қазақстан", "correct": true},
+//       {"optionName": "Арал маңы", "correct": true},
+//       {"optionName": "Оңтүстік Қазақстан", "correct": false},
+//       {"optionName": "Жетісу", "correct": false}
+//     ]
+//   }
+// ];
 
 // var questions = [];
 
@@ -30,8 +52,8 @@ var scoreCount = 0;
 var app = new Vue({
   el: '#app',
   data: {
-    // questions: [],
-    questions: questions,
+    questions: [],
+    // questions: questions,
     activeElement: 0,
     sendResultShow: false
   },
@@ -144,4 +166,4 @@ var app = new Vue({
   }
 });
 
-// app.getQuestions;
+app.getQuestions;
--- js/main.js
+++ js/main.js
@@ -1,41 +1,41 @@
-var questions = [
-  {
-    "id": "1",
-    "question": "Тегістегіш-жоңғыштар кездесетін неолиттік Қараүңгір тұрағы табылған аймақ",
-    "type": "radio",
-    "options": [
-      {"optionName": "1Шығыс Қазақстан", "correct": false},
-      {"optionName": "Орталық Қазақстан", "correct": false},
-      {"optionName": "Арал маңы", "correct": true},
-      {"optionName": "Оңтүстік Қазақстан", "correct": false},
-      {"optionName": "Жетісу", "correct": false}
-    ]
-  },
-  {
-    "id": "2",
-    "question": "Тегістегіш-жоңғыштар кездесетін неолиттік Қараүңгір тұрағы табылған аймақ",
-    "type": "radio",
-    "options": [
-      {"optionName": "2Шығыс Қазақстан", "correct": false},
-      {"optionName": "Орталық Қазақстан", "correct": false},
-      {"optionName": "Арал маңы", "correct": true},
-      {"optionName": "Оңтүстік Қазақстан", "correct": false},
-      {"optionName": "Жетісу", "correct": false}
-    ]
-  },
-  {
-    "id": "3",
-    "question": "Тегістегіш-жоңғыштар кездесетін неолиттік Қараүңгір тұрағы табылған аймақ",
-    "type": "checkbox",
-    "options": [
-      {"optionName": "4Шығыс Қазақстан", "correct": true},
-      {"optionName": "Орталық Қазақстан", "correct": true},
-      {"optionName": "Арал маңы", "correct": true},
-      {"optionName": "Оңтүстік Қазақстан", "correct": false},
-      {"optionName": "Жетісу", "correct": false}
-    ]
-  }
-];
+// var questions = [
+//   {
+//     "id": "1",
+//     "question": "Тегістегіш-жоңғыштар кездесетін неолиттік Қараүңгір тұрағы табылған аймақ",
+//     "type": "radio",
+//     "options": [
+//       {"optionName": "1Шығыс Қазақстан", "correct": false},
+//       {"optionName": "Орталық Қазақстан", "correct": false},
+//       {"optionName": "Арал маңы", "correct": true},
+//       {"optionName": "Оңтүстік Қазақстан", "correct": false},
+//       {"optionName": "Жетісу", "correct": false}
+//     ]
+//   },
+//   {
+//     "id": "2",
+//     "question": "Тегістегіш-жоңғыштар кездесетін неолиттік Қараүңгір тұрағы табылған аймақ",
+//     "type": "radio",
+//     "options": [
+//       {"optionName": "2Шығыс Қазақстан", "correct": false},
+//       {"optionName": "Орталық Қазақстан", "correct": false},
+//       {"optionName": "Арал маңы", "correct": true},
+//       {"optionName": "Оңтүстік Қазақстан", "correct": false},
+//       {"optionName": "Жетісу", "correct": false}
+//     ]
+//   },
+//   {
+//     "id": "3",
+//     "question": "Тегістегіш-жоңғыштар кездесетін неолиттік Қараүңгір тұрағы табылған аймақ",
+//     "type": "checkbox",
+//     "options": [
+//       {"optionName": "4Шығыс Қазақстан", "correct": true},
+//       {"optionName": "Орталық Қазақстан", "correct": true},
+//       {"optionName": "Арал маңы", "correct": true},
+//       {"optionName": "Оңтүстік Қазақстан", "correct": false},
+//       {"optionName": "Жетісу", "correct": false}
+//     ]
+//   }
+// ];
 
 // var questions = [];
 
@@ -50,8 +50,8 @@ var scoreCount = 0;
 var app = new Vue({
   el: '#app',
   data: {
-    // questions: [],
-    questions: questions,
+    questions: [],
+    // questions: questions,
     activeElement: 0,
     sendResultShow: false
   },
@@ -166,6 +166,6 @@ var app = new Vue({
   }
 });
 
-// app.getQuestions;
+app.getQuestions;
 
 
